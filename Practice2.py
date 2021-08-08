67165414c97d09a93ca443548e90b76cc5e6e0b8
mahmedjilani <82117986+mahmedjilani@users.noreply.github.com>
2021-08-08 05:06:14 -0400

    Challenge

---
+++ Practice2.py
@@ -0,0 +1,7 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Set up Splinter
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
